1217554cc5975ab9ea78a3726cccdaaf2d08a1d2
zack <zphricz@gmail.com>
2015-05-27 14:19:18 -0400

    - Updated to latest Threadpool - Expanded qsort to work on everything that implements []

--- main.cpp
+++ main.cpp
@@ -1,36 +1,42 @@
-#include <cstdlib>
 #include <iostream>
+#include <vector>
 #include "qsort.h"
 
-void randomize(int arr[], int size) {
-    for (int i = 0; i < size; ++i) {
-        arr[i] = rand();
-    }
-}
-
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        cout << "Input a size" << endl;
-        return -1;
-    }
-    srand(time(NULL));
-    int size = atoi(argv[1]);
-    int *arr = new int[size];
-    randomize(arr, size);
-    std::chrono::high_resolution_clock::time_point start, end;
-    start = std::chrono::high_resolution_clock::now();
-    qsort(arr, size);
-    end = std::chrono::high_resolution_clock::now();
-    if (!is_sorted(arr, size)) {
-        cout << "FAILURE" << endl;
-    } else {
-        cout << "SUCCESS" << endl;
-    }
+  if (argc != 2) {
+    std::cout << "Input a size" << std::endl;
+    return -1;
+  }
+  int size = atoi(argv[1]);
+  int *arr = new int[size];
+  randomize(arr, size);
+  std::chrono::high_resolution_clock::time_point start, end;
+  start = std::chrono::high_resolution_clock::now();
+  qsort(arr, size);
+  end = std::chrono::high_resolution_clock::now();
+  if (!is_sorted(arr, size)) {
+    std::cout << "FAILURE" << std::endl;
+  } else {
+    std::cout << "SUCCESS" << std::endl;
+  }
+  std::cout << "ARRAY TIME: "
+     << std::chrono::duration_cast<std::chrono::duration<float> >(
+        end - start).count() << " seconds" << std::endl;
+  delete[] arr;
 
-    delete[] arr;
-    cout << "TIME: "
-         << std::chrono::duration_cast<std::chrono::duration<float> >(
-                end - start).count() << " seconds" << endl;
-
-    return 0;
+  std::vector<int> v;
+  v.resize(size);
+  randomize(v);
+  start = std::chrono::high_resolution_clock::now();
+  qsort(v);
+  end = std::chrono::high_resolution_clock::now();
+  if (!is_sorted(v)) {
+    std::cout << "FAILURE" << std::endl;
+  } else {
+    std::cout << "SUCCESS" << std::endl;
+  }
+  std::cout << "VECTOR TIME: "
+     << std::chrono::duration_cast<std::chrono::duration<float> >(
+        end - start).count() << " seconds" << std::endl;
+  return 0;
 }
--- qsort.h
+++ qsort.h
@@ -1,26 +1,25 @@
 #ifndef QSORT_H
 #define QSORT_H
 
+#include <random>
 #include "Threadpool.h"
-
-using namespace std;
 
 constexpr int SERIAL_CUTOFF = 1024;
 constexpr int INSERTION_CUTOFF = 32;
 
 template <typename T>
-void partition(T *l, int start, int end, int &run_start, int &run_end) {
-  T pval = l[(end + start) / 2];
+void partition(T &list, int start, int end, int &run_start, int &run_end) {
+  auto pval = list[(end + start) / 2];
   int i = start;
   run_start = start;
   run_end = end;
   while (i <= run_end) {
-    if (l[i] < pval) {
-      swap(l[i], l[run_start]);
+    if (list[i] < pval) {
+      std::swap(list[i], list[run_start]);
       run_start++;
       i++;
-    } else if (l[i] > pval) {
-      swap(l[i], l[run_end]);
+    } else if (list[i] > pval) {
+      std::swap(list[i], list[run_end]);
       run_end--;
     } else {
       i++;
@@ -29,57 +28,82 @@ void partition(T *l, int start, int end, int &run_start, int &run_end) {
 }
 
 template <typename T>
-void insertion(T *l, int size) {
-  for (int i = 1; i < size; ++i) {
-    T key = l[i];
+void insertion(T &list, int start, int end) {
+  for (int i = start + 1; i <= end; ++i) {
+    auto key = list[i];
     int k = i;
-    while (k > 0 && key < l[k - 1]) {
-      l[k] = l[k - 1];
+    while (k > 0 && key < list[k - 1]) {
+      list[k] = list[k - 1];
       k--;
     }
-    l[k] = key;
+    list[k] = key;
   }
 }
 
 template <typename T>
-void serial_qsort_help(T *l, int start, int end) {
+void serial_qsort_help(T &list, int start, int end) {
   int run_start;
   int run_end;
   if (end - start <= INSERTION_CUTOFF - 1) {
-    insertion(l + start, end - start + 1);
+    insertion(list, start, end);
     return;
   }
-  partition(l, start, end, run_start, run_end);
-  serial_qsort_help(l, start, run_start - 1);
-  serial_qsort_help(l, run_end + 1, end);
+  partition(list, start, end, run_start, run_end);
+  serial_qsort_help(list, start, run_start - 1);
+  serial_qsort_help(list, run_end + 1, end);
 }
 
 template <typename T>
-void qsort_help(T *l, int start, int end) {
+void qsort_help(T &list, int start, int end) {
   if (end - start <= SERIAL_CUTOFF - 1) {
-    serial_qsort_help(l, start, end);
+    serial_qsort_help(list, start, end);
     return;
   }
   int run_start;
   int run_end;
-  partition(l, start, end, run_start, run_end);
-  submit_task(qsort_help<T>, l, start, run_start - 1);
-  submit_task(qsort_help<T>, l, run_end + 1, end);
+  partition(list, start, end, run_start, run_end);
+  submit_task(qsort_help<T>, std::ref(list), start, run_start - 1);
+  submit_task(qsort_help<T>, std::ref(list), run_end + 1, end);
 }
 
 template <typename T>
-void qsort(T *l, int size) {
-  qsort_help(l, 0, size - 1);
+void qsort(T &list, int size) {
+  qsort_help(list, 0, size - 1);
   wait_for_all_jobs();
 }
 
+// T must implement the size() method to work
 template <typename T>
-bool is_sorted(T *l, int size) {
+void qsort(T &list) {
+  qsort(list, list.size());
+}
+
+template <typename T>
+bool is_sorted(T &list, int size) {
   for (int i = 0; i < size - 1; ++i) {
-    if (l[i] > l[i + 1])
+    if (list[i] > list[i + 1])
       return false;
   }
   return true;
+}
+
+template <typename T>
+bool is_sorted(T &list) {
+  return is_sorted(list, list.size());
+}
+
+template <typename T>
+void randomize(T &list, int size) {
+  std::mt19937 gen;
+  std::uniform_int_distribution<int> dis;
+  for (int i = 0; i < size; ++i) {
+    list[i] = dis(gen);
+  }
+}
+
+template <typename T>
+void randomize(T &list) {
+  randomize(list, list.size());
 }
 
 #endif
